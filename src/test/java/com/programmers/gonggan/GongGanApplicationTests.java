a125b4a41e56700a95db8a373eb0291427ee6ad6
KyungMin Lee <hello.tidavid@gmail.com>
2023-11-02 21:33:38 +0900

    INIT: Initial Commit

---
+++ src/test/java/com/programmers/gonggan/GongGanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.gonggan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongGanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
